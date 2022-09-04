b01a6991ad2496255a15ae0e7c7d78aec75e6e62
YucenLyc <yucen.u@gmail.com>
2022-09-04 16:16:07 -0700

    set up the get route to connect to spotify

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
+
 app.get('/', (req, res) => {
   res.send('Hello It\'s Buzzie\'s World!');
+});
+
+app.get('/login', (req, res) => {
+  res.redirect(`https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${REDIRECT_URI}`);
 });
 
 const port = 9999;
