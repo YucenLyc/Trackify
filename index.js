7d9f6c0f058c11c2029f210dbac5963e643372ab
YucenLyc <yucen.u@gmail.com>
2022-09-07 21:21:17 -0700

    add state & scope query params

--- index.js
+++ index.js
@@ -12,11 +12,35 @@ app.get('/', (req, res) => {
   res.send('Hello It\'s Buzzie\'s World!');
 });
 
+/**
+ * Generates a random string containing numbers and letters
+ * @param  {number} length The length of the string
+ * @return {string} The generated string
+ */
+ const generateRandomString = length => {
+  let text = '';
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
+
+const stateKey = 'spotify_auth_state';
+
 app.get('/login', (req, res) => {
+  const state = generateRandomString(16);
+  //set a cookie: 
+  res.cookie(stateKey, state);
+
+  const scope ='user-read-private user-read-email';
+
   const queryParams = querystring.stringify({
     client_id: CLIENT_ID,
     response_type: 'code', 
     redirect_uri: REDIRECT_URI,
+    state: state,
+    scope: scope
   })
 
 
