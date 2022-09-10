c9618ffe0c4773cb59ec43c61174eb2f9302bb3e
YucenLyc <yucen.u@gmail.com>
2022-09-10 16:39:46 -0700

    set up /refresh_token handler

--- index.js
+++ index.js
@@ -67,7 +67,6 @@ app.get('/callback', (req, res) => {
   })
   .then(response => {
     if (response.status === 200) {
-
       const { access_token, token_type } = response.data;
 
       axios.get('https://api.spotify.com/v1/me', {
@@ -89,7 +88,46 @@ app.get('/callback', (req, res) => {
   .catch(error => {
     res.send(error);
   });
+  //     const { refresh_token } = response.data;
+
+  //     axios.get(`http://localhost:9999/refresh_token?refresh_token=${refresh_token}`)
+  //       .then(response => {
+  //         res.send(`<pre>${JSON.stringify(response.data, null, 2)}</pre>`);
+  //       })
+  //       .catch(error => {
+  //         res.send(error);
+  //       });
+  //   } else {
+  //     res.send(response);
+  //   }
+  // })
+  // .catch(error => {
+  //   res.send(error);
+  // });
 })
+
+app.get('/refresh_token', (req, res) => {
+  const { refresh_token } = req.query;
+
+  axios({
+    method: 'post',
+    url: 'https://accounts.spotify.com/api/token',
+    data: querystring.stringify({
+      grant_type: 'refresh_token',
+      refresh_token: refresh_token
+    }),
+    headers: {
+      'content-type': 'application/x-www-form-urlencoded',
+      Authorization: `Basic ${new Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64')}`,
+    },
+  })
+    .then(response => {
+      res.send(response.data);
+    })
+    .catch(error => {
+      res.send(error);
+    });
+});
 
 const port = 9999;
 app.listen(port, () => {
