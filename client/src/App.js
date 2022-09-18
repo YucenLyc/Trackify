a6c72c93519aebab5dc9ae17bbc06402326b94fe
YucenLyc <yucen.u@gmail.com>
2022-09-18 16:51:06 -0700

    separation of concerns

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,27 @@
 import logo from './logo.svg';
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
+import { accessToken } from './spotify';
 import './App.css';
 
 function App() {
+  const [token, setToken] = useState(null);
+
   useEffect(() => {
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    const accessToken = urlParams.get('access_token');
-    const refreshToken = urlParams.get('refresh_token');
-
-    console.log("this is aceess_token:", accessToken);
-    console.log("this is refresh_token:", refreshToken);
-
-    if(refreshToken) {
-      fetch(`/refresh_token?refresh_token=${refreshToken}`)
-      .then(res => res.json())
-      .then(data => console.log(data))
-      .catch(err => console.log(err))
-    }
+    setToken(accessToken);
   }, []);
 
- 
-  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="http://localhost:9999/login">
-          Log in to Spotify
-        </a>
+        {!token ? (
+          < a className="App-link" href="http://localhost:9999/login">
+            Log in to Spotify
+          </a>
+        ) : (
+          <h1>Logged In!</h1>
+        )}
       </header>
-    </div>
+    </div >
   );
 }
 
