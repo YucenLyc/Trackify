b24dbc1cb1b11247aa041e054e3b9992e5d98de5
YucenLyc <yucen.u@gmail.com>
2022-09-20 15:19:52 -0700

    create a getCurrentUserProfile funtion

--- client/src/spotify.js
+++ client/src/spotify.js
@@ -106,3 +106,5 @@ export const accessToken = getAccessToken();
 axios.defaults.baseURL = 'https://api.spotify.com/v1';
 axios.defaults.headers['Authorization'] = `Bearer ${accessToken}`;
 axios.defaults.headers['Content-Type'] = 'application/json';
+
+export const getCurrentUserProfile = () => axios.get('/me');
