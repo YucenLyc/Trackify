d76957d81beae19466f1321362795f90a90f4ec5
YucenLyc <yucen.u@gmail.com>
2022-10-08 16:34:44 -0700

    create ArtistGrid component & create files for SectionWrapper and index.js for components directory

---
+++ client/src/components/ArtistsGrid.js
@@ -0,0 +1,26 @@
+import StyledGrid from "../styles";
+
+const ArtistsGrid = ({ artists }) => (
+  <>
+    {artists && artists.length ? (
+      <StyledGrid type="artists">
+        {artists.map((artist, i) => (
+          <li className="grid__item" key={i}>
+            <div className="grid__item__inner">{artist.images[0] && (
+              <div className="grid__item__img">
+                <img src={artist.images[0].url} alt={artist.name} />
+              </div>
+            )}
+              <h3 className="grid__item__name overflow-ellipsis">{artist.name}</h3>
+              <p className="grid__item__label">Artist</p>
+            </div>
+          </li>
+        ))}
+      </StyledGrid>
+    ) : (
+      <p className="empty-notic">No Artists Available</p>
+    )}
+  </>
+);
+
+export default ArtistsGrid; 
---
+++ client/src/components/SectionWrapper.js
@@ -0,0 +1,3 @@
+import {Link} from 'react-router-dom';
+import { StyledSection } from '../styles';
+
