b8f743bd1a38f2c03c8e62db20d0e28a8cf10012
YucenLyc <yucen.u@gmail.com>
2022-10-23 17:55:11 -0700

    Import the component to client/src/pages/Profile.js and add it to the template

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,8 +1,18 @@
 import { useState, useEffect } from 'react';
 import { catchErrors } from '../utils';
-import { getCurrentUserProfile, getCurrentUserPlaylists, getTopArtists, getTopTracks } from '../spotify';
+import {
+  getCurrentUserProfile,
+  getCurrentUserPlaylists,
+  getTopArtists,
+  getTopTracks
+} from '../spotify';
+import {
+  SectionWrapper,
+  ArtistsGrid,
+  TrackList,
+  PlaylistsGrid
+} from '../components';
 import { StyledHeader } from '../styles';
-import { SectionWrapper, ArtistsGrid, TrackList, PlaylistsGrid } from '../components'
 
 const Profile = () => {
   const [profile, setProfile] = useState(null);
@@ -12,8 +22,8 @@ const Profile = () => {
 
   useEffect(() => {
     const fetchData = async () => {
-      const UserProfile = await getCurrentUserProfile();
-      setProfile(UserProfile.data);
+      const userProfile = await getCurrentUserProfile();
+      setProfile(userProfile.data);
 
       const userPlaylists = await getCurrentUserPlaylists();
       setPlaylists(userPlaylists.data);
@@ -23,14 +33,10 @@ const Profile = () => {
 
       const userTopTracks = await getTopTracks();
       setTopTracks(userTopTracks.data);
-
-      console.log("hello top tracks", userTopTracks.data)
-    }
+    };
 
     catchErrors(fetchData());
   }, []);
-
-  //console.log("this is what you're looking for", topArtists);
 
   return (
     <>
@@ -39,28 +45,30 @@ const Profile = () => {
           <StyledHeader type="user">
             <div className="header__inner">
               {profile.images.length && profile.images[0].url && (
-                <img className="header__img" src={profile.images[0].url} alt="Avatar" />
+                <img className="header__img" src={profile.images[0].url} alt="Avatar"/>
               )}
-              <div className="header__overline">Profile
+              <div>
+                <div className="header__overline">Profile</div>
                 <h1 className="header__name">{profile.display_name}</h1>
                 <p className="header__meta">
                   {playlists && (
                     <span>{playlists.total} Playlist{playlists.total !== 1 ? 's' : ''}</span>
                   )}
-                  <span>{profile.followers.total} Follower{profile.followers.total !== 1 ? 's' : ''}
+                  <span>
+                    {profile.followers.total} Follower{profile.followers.total !== 1 ? 's' : ''}
                   </span>
                 </p>
               </div>
             </div>
           </StyledHeader>
-          {topArtists && topTracks &&(
+
+          {topArtists && topTracks && playlists && (
             <main>
               <SectionWrapper title="Top artists this month" seeAllLink="/top-artists">
                 <ArtistsGrid artists={topArtists.items.slice(0, 10)} />
               </SectionWrapper>
 
-              <SectionWrapper title="Top tracks this month" seeAllLink="/
-              top-tracks">
+              <SectionWrapper title="Top tracks this month" seeAllLink="/top-tracks">
                 <TrackList tracks={topTracks.items.slice(0, 10)} />
               </SectionWrapper>
 
@@ -73,6 +81,6 @@ const Profile = () => {
       )}
     </>
   )
-}
+};
 
 export default Profile;
